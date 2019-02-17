9d9cc85bcd2ceb27c1b4d850b10ca1d72c187f72
abcparadise <dj673344908@163.com>
2019-02-17 17:44:14 +0800

    后台管理初始化

---
+++ taotao-manager-web/src/main/java/com/dj/taotao/controller/PageController.java
@@ -0,0 +1,24 @@
+package com.dj.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+/**
+ * 
+ * @author dj673
+ *
+ */
+@Controller
+public class PageController {
+
+	@RequestMapping("/")
+	public String showIndex() {
+		return "index";
+	}
+	
+	@RequestMapping("/{page}")
+	public String showPage(@PathVariable String page) {
+		return page;
+	}
+	
+}
