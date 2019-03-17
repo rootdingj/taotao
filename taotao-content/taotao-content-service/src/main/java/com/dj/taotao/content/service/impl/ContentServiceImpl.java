d3b3d2c7b914b060ab9cc93e5f4eaac0f02cbeab
abcparadise <dj673344908@163.com>
2019-03-17 16:25:55 +0800

    商城首页的缓存应用

--- taotao-content/taotao-content-service/src/main/java/com/dj/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content/taotao-content-service/src/main/java/com/dj/taotao/content/service/impl/ContentServiceImpl.java
@@ -3,21 +3,29 @@ package com.dj.taotao.content.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.dj.taotao.content.service.ContentService;
+import com.dj.taotao.jedis.JedisClient;
 import com.dj.taotao.mapper.TbContentMapper;
 import com.dj.taotao.pojo.TaotaoResult;
 import com.dj.taotao.pojo.TbContent;
 import com.dj.taotao.pojo.TbContentExample;
 import com.dj.taotao.pojo.TbContentExample.Criteria;
+import com.dj.taotao.utils.JsonUtils;
 
 @Service
 public class ContentServiceImpl implements ContentService {
 
 	@Autowired
 	private TbContentMapper contentMapper;
+	@Autowired
+	private JedisClient jedisClient;
+	@Value("${INDEX_CONTENT}")
+	private String indexContent;
 
 	@Override
 	public TaotaoResult addContent(TbContent content) {
@@ -25,15 +33,35 @@ public class ContentServiceImpl implements ContentService {
 		content.setCreated(date);
 		content.setUpdated(date);
 		contentMapper.insert(content);
+		// 同步缓存，删除key对应的缓存信息
+		jedisClient.hdel(indexContent, content.getCategoryId().toString());
 		return TaotaoResult.ok();
 	}
 
 	@Override
 	public List<TbContent> getContentByCid(long cid) {
+		// 先查询缓存
+		try {
+			// 查询缓存，并把查询结果转成list返回
+			String json = jedisClient.hget(indexContent, cid + "");
+			if (StringUtils.isNotBlank(json)) {
+				List<TbContent> list = JsonUtils.jsonToList(json, TbContent.class);
+				return list;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		TbContentExample example = new TbContentExample();
 		Criteria criteria = example.createCriteria();
 		criteria.andCategoryIdEqualTo(cid);
-		return contentMapper.selectByExample(example);
+		List<TbContent> list = contentMapper.selectByExample(example);
+		// 如果缓存中不存在，先查询数据库，再把结果存入缓存
+		try {
+			jedisClient.hset(indexContent, cid + "", JsonUtils.objectToJson(list));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
 	}
 
 }
--- taotao-content/taotao-content-service/src/test/java/com/dj/taotao/TestJdedis.java
+++ taotao-content/taotao-content-service/src/test/java/com/dj/taotao/TestJdedis.java
@@ -1,8 +1,17 @@
 package com.dj.taotao;
 
-import org.junit.Test;
+import java.util.HashSet;
+import java.util.Set;
 
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.dj.taotao.jedis.JedisClient;
+
+import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPool;
 
 public class TestJdedis {
@@ -27,7 +36,37 @@ public class TestJdedis {
 		jedis.close();
 		// 系统关闭前关闭连接池
 		pool.close();
+	}
 
+	@Test
+	public void testJedisCluster() {
+		// 创建一个JedisCluster对象，构造参数set类型，集合中每个元素是PostAndPort类型
+		Set<HostAndPort> nodes = new HashSet<>();
+		nodes.add(new HostAndPort("47.106.125.36", 7001));
+		nodes.add(new HostAndPort("47.106.125.36", 7002));
+		nodes.add(new HostAndPort("47.106.125.36", 7003));
+		nodes.add(new HostAndPort("47.106.125.36", 7004));
+		nodes.add(new HostAndPort("47.106.125.36", 7005));
+		nodes.add(new HostAndPort("47.106.125.36", 7006));
+		JedisCluster jedisCluster = new JedisCluster(nodes);
+		// 直接使用jedisCluster对象操作redis，自带连接池，jedisCluster对象可以是单例的
+		jedisCluster.set("JedisCluster", "123456");
+		System.out.println(jedisCluster.get("JedisCluster"));
+		jedisCluster.close();
+
+	}
+	
+	@Test
+	public void testJedisClient() {
+		// 初始化Spring容器
+		ApplicationContext applicationContext = 
+				new ClassPathXmlApplicationContext("classpath:spring/applicationContext-jedis.xml");
+		// 获取jedisClient对象
+		JedisClient jedisClient = applicationContext.getBean(JedisClient.class);
+		//使用JedisClient对象操作redis
+		jedisClient.set("jedisclient", "mytest");
+		System.out.println("获取数据： "+jedisClient.get("jedisclient"));
+		
 	}
 
 }
