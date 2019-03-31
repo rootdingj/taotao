5e8eb1c005e3344f31adf41aefb9a558df481007
abcparadise <dj673344908@163.com>
2019-03-31 15:51:02 +0800

    商品详情页面静态化

---
+++ taotao-item-web/src/main/java/com/dj/taotao/item/controller/HtmlGenController.java
@@ -0,0 +1,39 @@
+package com.dj.taotao.item.controller;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+public class HtmlGenController {
+
+	@Autowired
+	private FreeMarkerConfigurer freeMarkerConfigurer;
+	@Value("${HTML_OUT_PATH}")
+	private String HTML_OUT_PATH;
+	
+	@RequestMapping("/genhtml")
+	@ResponseBody
+	public String genHtml() throws Exception {
+		//生成静态页面
+		Configuration configuration = freeMarkerConfigurer.getConfiguration();
+		Template template = configuration.getTemplate("hello.ftl");
+		Map data = new HashMap<>();
+		data.put("hello", "spring freemarker test");
+		Writer out = new FileWriter(new File(HTML_OUT_PATH+"test.html"));
+		template.process(data, out);
+		out.close();
+		//返回结果
+		return "OK";
+	}
+}
