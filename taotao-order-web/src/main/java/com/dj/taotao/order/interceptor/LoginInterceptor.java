17bb3ca2093024c8ee106e7327033db59bd1afb9
abcparadise <dj673344908@163.com>
2019-04-20 17:20:45 +0800

    登录拦截器实现

--- taotao-order-web/src/main/java/com/dj/taotao/order/interceptor/LoginInterceptor.java
+++ taotao-order-web/src/main/java/com/dj/taotao/order/interceptor/LoginInterceptor.java
@@ -3,22 +3,66 @@ package com.dj.taotao.order.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.dj.taotao.pojo.TaotaoResult;
+import com.dj.taotao.pojo.TbUser;
+import com.dj.taotao.sso.service.UserService;
+import com.dj.taotao.utils.CookieUtils;
+
 /**
  * 
- * @ClassName: LoginInterceptor 
+ * @ClassName: LoginInterceptor
  * @Description: 判断用户登录拦截器
- * @author Steven 
+ * @author Steven
  * @date 2019年4月13日
  */
 public class LoginInterceptor implements HandlerInterceptor {
 
+	@Value("${TOKEN_KEY}")
+	private String TOKEN_KEY;
+	@Value("${SSO_URL}")
+	private String SSO_URL;
+	
+	@Autowired
+	private UserService userService;
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		// TODO Auto-generated method stub
-		return false;
+		// 执行handler之前先执行此方法
+		// 1.从cookie中取token信息
+		String token = CookieUtils.getCookieValue(request, TOKEN_KEY);
+		// 2.如果取不到token，跳转到sso的登录页面，需要把当前请求的url做为参数传递给sso，sso登录成功之后跳转回请求的页面。
+		if (StringUtils.isBlank(token)) {
+			// 取当前请求的url
+			String requestURL = request.getRequestURL().toString();
+			// 跳转到登录页面
+			response.sendRedirect(SSO_URL + "/page/login?url=" + requestURL);
+			// 拦截
+			return false;
+		}
+		// 3.取到token，调用sso系统的服务判断用户是否登录
+		TaotaoResult taotaoResult = userService.getUserByToken(token);
+		// 4.如果用户未登录，即没取到用户信息。跳转到sso的登录页面
+		if (taotaoResult.getStatus() != 200) {
+			// 取当前请求的url
+			String requestURL = request.getRequestURL().toString();
+			// 跳转到登录页面
+			response.sendRedirect(SSO_URL + "/page/login?url=" + requestURL);
+			// 拦截
+			return false;
+		}
+		// 5.如果取到用户信息。放行。
+		// 把用户信息放到request中
+		TbUser user = (TbUser) taotaoResult.getData();
+		request.setAttribute("user", user);
+		// 返回值true：放行, 返回false：拦截
+		return true;
 	}
 
 	@Override
--- taotao-sso-web/src/main/java/com/dj/taotao/sso/controller/PageController.java
+++ taotao-sso-web/src/main/java/com/dj/taotao/sso/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.dj.taotao.sso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -17,8 +18,10 @@ public class PageController {
 	public String showRegister() {
 		return "register";
 	}
+	
 	@RequestMapping("/page/login")
-	public String showLogin() {
+	public String showLogin(String url, Model model) {
+		model.addAttribute("redirect", url);
 		return "login";
 	}
 
